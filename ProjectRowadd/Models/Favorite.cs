e6e51a7d5000a7a392b3bb308822102ad5384c4c
kholoudkhaled191 <kholoud.kh.mohamed1512@gmail.com>
2026-04-17 13:49:03 +0200

    edit

--- ProjectRowadd/Models/Favorite.cs
+++ ProjectRowadd/Models/Favorite.cs
@@ -2,5 +2,9 @@
 {
     public class Favorite
     {
+        public int FavoriteId { get; set; }
+        public int CustomerId { get; set; }
+        public int WorkerId { get; set; }
+        public DateTime addedAt { get; set; }
     }
 }
